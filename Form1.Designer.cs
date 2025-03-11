ca1d932deb4b40440b3fd842f49df1bfddb07fd1
Stuart100594 <100594@gllm.ac.uk>
2025-03-11 13:26:10 +0000

    created login page 4
    added logo onto login page to complete the form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,14 +38,16 @@
             this.btnLogin = new System.Windows.Forms.Button();
             this.txtBoxUname = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.picBoxHide = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.picBoxShow = new System.Windows.Forms.PictureBox();
+            this.picBoxHide = new System.Windows.Forms.PictureBox();
             this.picBoxMini = new System.Windows.Forms.PictureBox();
             this.picBoxClose = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picBoxHide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxShow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHide)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxMini)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxClose)).BeginInit();
             this.SuspendLayout();
@@ -129,19 +131,15 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Username";
             // 
-            // picBoxHide
+            // pictureBox1
             // 
-            this.picBoxHide.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.picBoxHide.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.picBoxHide.Image = global::BayWynCouriers.Properties.Resources.eyeClose;
-            this.picBoxHide.Location = new System.Drawing.Point(437, 222);
-            this.picBoxHide.Name = "picBoxHide";
-            this.picBoxHide.Size = new System.Drawing.Size(30, 30);
-            this.picBoxHide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.picBoxHide.TabIndex = 5;
-            this.picBoxHide.TabStop = false;
-            this.picBoxHide.Click += new System.EventHandler(this.picBoxHide_Click);
-            this.picBoxHide.MouseHover += new System.EventHandler(this.picBoxHide_MouseHover);
+            this.pictureBox1.Image = global::BayWynCouriers.Properties.Resources.BayWynLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(820, 102);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(650, 650);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // picBoxShow
             // 
@@ -156,6 +154,20 @@
             this.picBoxShow.TabStop = false;
             this.picBoxShow.Click += new System.EventHandler(this.picBoxShow_Click);
             this.picBoxShow.MouseHover += new System.EventHandler(this.picBoxShow_MouseHover);
+            // 
+            // picBoxHide
+            // 
+            this.picBoxHide.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBoxHide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picBoxHide.Image = global::BayWynCouriers.Properties.Resources.eyeClose;
+            this.picBoxHide.Location = new System.Drawing.Point(437, 222);
+            this.picBoxHide.Name = "picBoxHide";
+            this.picBoxHide.Size = new System.Drawing.Size(30, 30);
+            this.picBoxHide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxHide.TabIndex = 5;
+            this.picBoxHide.TabStop = false;
+            this.picBoxHide.Click += new System.EventHandler(this.picBoxHide_Click);
+            this.picBoxHide.MouseHover += new System.EventHandler(this.picBoxHide_MouseHover);
             // 
             // picBoxMini
             // 
@@ -197,6 +209,7 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.picBoxMini);
             this.Controls.Add(this.picBoxClose);
+            this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -208,8 +221,9 @@
             this.panel1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picBoxHide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxShow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHide)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxMini)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxClose)).EndInit();
             this.ResumeLayout(false);
@@ -231,6 +245,7 @@
         private System.Windows.Forms.TextBox txtBoxUname;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox picBoxHide;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace BayWynCouriers.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap BayWynLogo {
+            get {
+                object obj = ResourceManager.GetObject("BayWynLogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap close_window {
             get {
                 object obj = ResourceManager.GetObject("close-window", resourceCulture);
